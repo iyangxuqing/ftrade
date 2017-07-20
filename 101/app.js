54636a84fecb3ab691674b4fd67563b90e1d4688
iyangxuqing <iyangxuqing.qq.com>
2017-07-20 20:01:46 +0800

    loading.js

--- 101/app.js
+++ 101/app.js
@@ -1,6 +1,7 @@
-import 'utils/util.js'
 let config = require('utils/config.js')
+import 'utils/util.js'
 import { http } from 'utils/http.js'
+import { Listener } from 'utils/listener.js'
 
 App({
   onLaunch: function () {
